b1f39782addf28b558184a109fc02afd70f4e167
PD Jell <mystral7@naver.com>
2021-04-22 11:40:40 +0900

    add tslint, tsconfig, index.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
+/**
+ * @from https://velog.io/@das01063/VSCode%EC%97%90%EC%84%9C-ESLint%EC%99%80-Prettier-TypeScript-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
+ */
+
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
+  //   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  //   parserOptions: {
+  //     project: './tsconfig.json',
+  //     ecmaVersion: 2018,
+  //     sourceType: 'module'
+  //   },
+  ignorePatterns: ['dist/', 'node_modules/']
 }
