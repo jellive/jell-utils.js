1fb843355a07d939345bce0945442964a94aba00
PD Jell <mystral7@naver.com>
2021-04-23 11:36:03 +0900

    add desc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @description return message is korean
+ * @param message target string.
+ * @returns boolean
+ */
 const isKorean = (message: string): boolean => {
   const regex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/gi
   return regex.test(message)
