0b4d1c85c6c18b2fe18aef09fd6c34bcf6729c06
PD Jell <mystral7@naver.com>
2021-04-29 16:42:20 +0900

    0.0.14

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// 커스텀 util에 관한 기능 정의
 const util = {
   /**
    * @description return message is korean
@@ -6,8 +5,8 @@ const util = {
    * @returns boolean
    */
   isKorean: (message: string): boolean => {
-    const regex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/gi;
-    return regex.test(message);
+    const regex = /[ㄱ-ㅎㅏ-ㅣ가-힣]/gi
+    return regex.test(message)
   },
 
   /**
@@ -20,14 +19,14 @@ const util = {
     // if (obj === null || typeof obj !== 'object') {
     //   return obj
     // }
-    const copy = obj.constructor() as Record<string, unknown>;
+    const copy = obj.constructor() as Record<string, unknown>
     for (const attr in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, attr)) {
         // from: https://ourcodeworld.com/articles/read/1425/how-to-fix-eslint-error-do-not-access-objectprototype-method-hasownproperty-from-target-object-no-prototype-builtins
-        copy[attr] = obj[attr];
+        copy[attr] = obj[attr]
       }
     }
-    return copy;
+    return copy
   },
   /**
    * @description encode uri parsing (like &lt; => <, &gt; => >)
@@ -35,7 +34,7 @@ const util = {
    * @returns tag parsed string
    */
   parseTag: (txt: string): string => {
-    return txt.replace(/&lt;/gi, "<").replace(/&gt;/gi, ">");
+    return txt.replace(/&lt;/gi, '<').replace(/&gt;/gi, '>')
   },
   /**
    * @description parseNumber with defaultvalue
@@ -48,11 +47,11 @@ const util = {
     defaultValue: number,
     isFloat = false
   ): number => {
-    if (!target || typeof parseInt(target) !== "number") {
-      return defaultValue;
+    if (!target || typeof parseInt(target) !== 'number') {
+      return defaultValue
     }
 
-    return isFloat ? parseFloat(target) : parseInt(target);
+    return isFloat ? parseFloat(target) : parseInt(target)
   },
   /**
    *
@@ -61,29 +60,29 @@ const util = {
    * @returns
    */
   parseTime: (target: string, defaultValue: number): number => {
-    if (!target.includes(":"))
-      return defaultValue ? defaultValue : parseInt(target);
+    if (!target.includes(':'))
+      return defaultValue ? defaultValue : parseInt(target)
 
-    const matched = target.match(/:/gi);
+    const matched = target.match(/:/gi)
     if (target && matched) {
-      const isMilliSecond = matched.length == 2;
-      const timeValues = target.split(":").map((value) => parseInt(value, 0));
-      let minute = 0;
-      let second = 0;
-      let milliSecond = 0;
+      const isMilliSecond = matched.length == 2
+      const timeValues = target.split(':').map(value => parseInt(value, 0))
+      let minute = 0
+      let second = 0
+      let milliSecond = 0
       if (isMilliSecond) {
-        minute = timeValues[0] || 0;
-        second = timeValues[1] || 0;
-        milliSecond = timeValues[2] || 0;
+        minute = timeValues[0] || 0
+        second = timeValues[1] || 0
+        milliSecond = timeValues[2] || 0
       } else {
-        minute = timeValues[0] || 0;
-        second = timeValues[1] || 0;
+        minute = timeValues[0] || 0
+        second = timeValues[1] || 0
       }
       const milliSeconds: number =
-        minute * 60 * 1000 + second * 1000 + milliSecond;
-      return milliSeconds;
+        minute * 60 * 1000 + second * 1000 + milliSecond
+      return milliSeconds
     } else {
-      return 0;
+      return 0
     }
   },
   /**
@@ -91,26 +90,26 @@ const util = {
    * @returns string now datetime format
    */
   getNowDate: (): string => {
-    const date = new Date();
+    const date = new Date()
     let dateString = new Date(
       date.getTime() - date.getTimezoneOffset() * 60000
-    ).toISOString();
-    dateString = dateString.replace("T", " ").split(".")[0];
+    ).toISOString()
+    dateString = dateString.replace('T', ' ').split('.')[0]
 
-    return dateString;
+    return dateString
   },
   /**
    * @description generate string from inputed time with korean date format.
    * @returns string korean date format
    */
   getKoreanDate: (dateString = new Date(), isYear = false): string => {
-    const targetDate = new Date(dateString);
-    const year = targetDate.getFullYear();
-    const month = `0${targetDate.getMonth() + 1}`.substr(-2);
-    const date = `0${targetDate.getDate()}`.substr(-2);
+    const targetDate = new Date(dateString)
+    const year = targetDate.getFullYear()
+    const month = `0${targetDate.getMonth() + 1}`.substr(-2)
+    const date = `0${targetDate.getDate()}`.substr(-2)
 
-    if (isYear) return `${year}년 ${month}월 ${date}일`;
-    return `${month}월 ${date}일`;
+    if (isYear) return `${year}년 ${month}월 ${date}일`
+    return `${month}월 ${date}일`
   },
   /**
    * @description convert date to yyyy-mm-dd format string.
@@ -118,14 +117,14 @@ const util = {
    * @returns yyyy-mm-dd formatted string.
    */
   formatDate: (date: Date | string = new Date()): string => {
-    const d = new Date(date);
-    let month = `${d.getMonth() + 1}`;
-    let day = `${d.getDate()}`;
-    const year = d.getFullYear();
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
+    const d = new Date(date)
+    let month = `${d.getMonth() + 1}`
+    let day = `${d.getDate()}`
+    const year = d.getFullYear()
+    if (month.length < 2) month = '0' + month
+    if (day.length < 2) day = '0' + day
 
-    return [year, month, day].join("-");
+    return [year, month, day].join('-')
   },
   /**
    * @description calculate d-day with target date.
@@ -133,12 +132,12 @@ const util = {
    * @returns d-day
    */
   calDDay: (date: Date | string = new Date()): number => {
-    const countDownDate = new Date(date);
-    const now = new Date().getTime();
-    const distance = countDownDate.getTime() - now;
+    const countDownDate = new Date(date)
+    const now = new Date().getTime()
+    const distance = countDownDate.getTime() - now
 
-    const countDown = Math.floor(distance / (1000 * 60 * 60 * 24));
-    return countDown;
+    const countDown = Math.floor(distance / (1000 * 60 * 60 * 24))
+    return countDown
   },
   /**
    * @description replace word in str with index.
@@ -156,8 +155,8 @@ const util = {
   ): string => {
     const newStr = `${str.substring(0, startIndex)}${txt}${str.substring(
       endIndex
-    )}`;
-    return newStr;
+    )}`
+    return newStr
   },
   /**
    * @description make camel cased word.
@@ -165,19 +164,19 @@ const util = {
    * @returns
    */
   toCamelCase: (txt: string): string => {
-    txt = typeof txt !== "string" ? "" : txt;
+    txt = typeof txt !== 'string' ? '' : txt
     const txtArr = txt.split(/_| /g).map((word, index) => {
-      let firstvarter;
+      let firstvarter
       if (index == 0) {
-        firstvarter = word.charAt(0).toLowerCase();
+        firstvarter = word.charAt(0).toLowerCase()
       } else {
-        firstvarter = word.charAt(0).toUpperCase();
+        firstvarter = word.charAt(0).toUpperCase()
       }
 
-      word = firstvarter.concat(word.substr(1));
-      return word;
-    });
-    return txtArr.join("");
+      word = firstvarter.concat(word.substr(1))
+      return word
+    })
+    return txtArr.join('')
   },
   /**
    * @description make snake cased word.
@@ -185,9 +184,9 @@ const util = {
    * @returns
    */
   toSnakeCase: (txt: string): string => {
-    txt = typeof txt !== "string" ? "" : txt;
-    txt = txt.toLowerCase().replace(/ /g, "_");
-    return txt;
+    txt = typeof txt !== 'string' ? '' : txt
+    txt = txt.toLowerCase().replace(/ /g, '_')
+    return txt
   },
   /**
    * @description make title case string(first character capital and word space)
@@ -195,28 +194,28 @@ const util = {
    * @returns
    */
   toTitleCase: (txt: string): string => {
-    txt = typeof txt !== "string" ? "" : txt;
+    txt = typeof txt !== 'string' ? '' : txt
 
     // snake case
-    txt = txt.replace(/_/g, " ");
+    txt = txt.replace(/_/g, ' ')
     txt = txt
-      .split(" ")
-      .map((word) => word.charAt(0).toUpperCase().concat(word.substr(1)))
-      .join(" ");
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase().concat(word.substr(1)))
+      .join(' ')
 
     // input space when camel case
-    const upperCases = txt.match(/[A-Z]/g) || [];
-    upperCases.forEach((varter) => {
-      const regEx = new RegExp(varter, "g");
-      txt = txt.replace(regEx, " " + varter);
-    });
+    const upperCases = txt.match(/[A-Z]/g) || []
+    upperCases.forEach(varter => {
+      const regEx = new RegExp(varter, 'g')
+      txt = txt.replace(regEx, ' ' + varter)
+    })
 
     while (/{2}/g.exec(txt) != null) {
-      txt = txt.replace(/{2}/g, " ");
+      txt = txt.replace(/{2}/g, ' ')
     }
-    txt = txt.trim();
+    txt = txt.trim()
 
-    return txt;
+    return txt
   },
   /**
    * @description br tag to line break.
@@ -224,7 +223,7 @@ const util = {
    * @returns
    */
   toText: (txt: string): string => {
-    return txt.replace(/<br>/gi, "\n");
+    return txt.replace(/<br>/gi, '\n')
   },
   /**
    * @description line break to br tag.
@@ -232,7 +231,7 @@ const util = {
    * @returns
    */
   toHtml: (txt: string): string => {
-    return txt.replace(/\n/gi, "<br>");
+    return txt.replace(/\n/gi, '<br>')
   },
   /**
    * @description remove tag brace text.
@@ -241,35 +240,35 @@ const util = {
    * @returns
    */
   clearTag: (txt: string, isError = false): string => {
-    const txtWrap = document.createElement("div");
-    txtWrap.innerHTML = txt;
+    const txtWrap = document.createElement('div')
+    txtWrap.innerHTML = txt
 
-    const tags = ["div", "font", "span"];
-    tags.forEach((tag) => {
-      let elements = txtWrap.querySelectorAll(tag);
-      let errorWraps = txtWrap.querySelectorAll(`${tag}.suggestCheck`);
+    const tags = ['div', 'font', 'span']
+    tags.forEach(tag => {
+      let elements = txtWrap.querySelectorAll(tag)
+      let errorWraps = txtWrap.querySelectorAll(`${tag}.suggestCheck`)
       let isContinue = isError
         ? elements.length - errorWraps.length > 0
-        : elements.length > 0;
+        : elements.length > 0
 
       while (isContinue) {
-        elements.forEach((element) => {
-          const isErrorWrap = element.className.includes("suggestCheck");
+        elements.forEach(element => {
+          const isErrorWrap = element.className.includes('suggestCheck')
           if (!isError || !isErrorWrap) {
-            element.outerHTML = element.innerHTML;
+            element.outerHTML = element.innerHTML
           }
-        });
+        })
 
-        elements = txtWrap.querySelectorAll(tag);
-        errorWraps = txtWrap.querySelectorAll(`${tag}.suggestCheck`);
+        elements = txtWrap.querySelectorAll(tag)
+        errorWraps = txtWrap.querySelectorAll(`${tag}.suggestCheck`)
         isContinue = isError
           ? elements.length - errorWraps.length > 0
-          : elements.length > 0;
+          : elements.length > 0
       }
-    });
+    })
 
-    const clearedTxt = txtWrap.innerHTML.replace(/<br>/gi, "\n");
-    return clearedTxt;
+    const clearedTxt = txtWrap.innerHTML.replace(/<br>/gi, '\n')
+    return clearedTxt
   },
   /**
    * @description check same between two array.
@@ -278,15 +277,14 @@ const util = {
    * @returns
    */
   equalArrays: (a: unknown[], b: unknown[]): boolean => {
-    if (a === b) return true;
-    if (!a || !b) return false;
-    if (a.length != b.length) return false;
+    if (a === b) return true
+    if (!a || !b) return false
+    if (a.length != b.length) return false
 
     for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) return false;
+      if (a[i] !== b[i]) return false
     }
-    return true;
-  },
-};
-
-export default util;
+    return true
+  }
+}
+export { util }
